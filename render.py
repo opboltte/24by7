226ecd1466b6e6ec9e530bd452c6dbe346003ef1
opboltte <namme9998+1@gmail.com>
2024-10-03 11:24:03 +0530

    Update render.py

--- render.py
+++ render.py
@@ -1,9 +1,19 @@
 import os
 import time
 from dotenv import load_dotenv
+from flask import Flask
 
 # Load environment variables from .env file
 load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)  # Change to your desired port
 
 # Get the environment variables
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
